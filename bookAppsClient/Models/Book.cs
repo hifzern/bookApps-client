d41e9c1a47950c8dbf7b1261793e07adf0af858f
adh1ka <hafizhadhi27@gmail.com>
2025-08-26 11:06:10 +0700

    add Models/Book.cs

--- bookAppsClient/Models/Book.cs
+++ bookAppsClient/Models/Book.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace bookAppsClient.Models
 {
-    internal class Book
+    public class Book
     {
+        [JsonPropertyName("id")] public int Id { get; set; }
+        [JsonPropertyName("title")] public string Title { get; set; } = "";
+        [JsonPropertyName("author")] public string Author { get; set; } = "";
+        [JsonPropertyName("year")] public int Year { get; set; }
     }
 }
